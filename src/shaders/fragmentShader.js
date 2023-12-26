a59407b9aa1618e6206c7c0e2f195b934c175c87
kagunecode <cesar.acb98@gmail.com>
2023-12-26 16:00:54 -0500

    Added the texture to fragmentShader

--- src/shaders/fragmentShader.js
+++ src/shaders/fragmentShader.js
@@ -6,7 +6,7 @@ varying vec2 vUv;
 void main() {
     vec4 color = texture2D(u_texture, vUv); 
     gl_FragColor = color;
-    gl_FragColor = vec4(u_progress, 0., 1., 1.);
+    //gl_FragColor = vec4(u_progress, 0., 1., 1.);
 }
 `;
 
