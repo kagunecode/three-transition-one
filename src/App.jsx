f1e49853de3e61107b40b5aedf08b481177e0a54
kagunecode <cesar.acb98@gmail.com>
2023-12-26 22:23:00 -0500

    Removed camera controls

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { Canvas } from "@react-three/fiber";
 import Frame from "./components/Frame";
-import { CameraControls } from "@react-three/drei";
 
 export default function App() {
   return (
