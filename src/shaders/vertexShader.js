fd167f8f3f31a1a654cb6260f5424a59c6be4126
kagunecode <cesar.acb98@gmail.com>
2023-12-26 22:23:58 -0500

    Removed pos.z unused and changed animation timing

--- src/shaders/vertexShader.js
+++ src/shaders/vertexShader.js
@@ -6,7 +6,6 @@ varying vec2 vUv;
 void main() {
     vec3 pos = position;
 
-    // pos.z = 0.1*sin(pos.x*20.0);
     float distance = length(uv - vec2(0.5));
     float maxDistance = length(vec2(0.5));
 
@@ -19,7 +18,7 @@ void main() {
 
     float secProgress = min(2.*u_progress, 2.*(1. - u_progress));
 
-    float zOffset = 4.0;
+    float zOffset = 6.0;
 
     float zProgress = mix(clamp(2.*u_progress, 0., 1.), clamp(1. - 2.*(1. - u_progress), 0., 1.), u_direction);
 
